2f34686c768d4947e8f9ec0ef4a506cfb4b695e0
Slavik Markelov <mastram.sm@gmail.com>
2026-05-17 08:57:31 +0200

    Update script.js

--- miraclev2/script.js
+++ miraclev2/script.js
@@ -98,7 +98,7 @@ function closeMenu() {
 var walletPubkey = null;
 var walletProvider = null;
 var TACO_MINT = '3kemsuKXgMGmDu7oASK9m2BKeFyGGyzDsyNgvrBtbrrr';
-var RPC = 'https://rpc.ankr.com/solana';
+var RPC = 'https://mainnet.helius-rpc.com/?api-key=PUBLIC';
 
 function showWalletModal() {
   if (walletPubkey) { disconnectWallet(); return; }
